0c5ca0132e1223a857dd13cf4b07cb6ec2695bce
mikeder <mikedernet@gmail.com>
2023-04-20 08:57:22 -0400

    initial commit

--- examples/manual_update.rs
+++ examples/manual_update.rs
@@ -10,10 +10,16 @@ struct PlayerCharacter;
 struct Speed(f32);
 
 #[derive(Component)]
-struct Hp { current: i32, max: i32 }
+struct Hp {
+    current: i32,
+    max: i32,
+}
 
 #[derive(Component)]
-struct Mp { current: i32, max: i32 }
+struct Mp {
+    current: i32,
+    max: i32,
+}
 
 #[derive(Component)]
 struct StatBars {
@@ -21,64 +27,79 @@ struct StatBars {
     pub mp: Entity,
 }
 
-fn spawn_player(
-    mut commands: Commands
-) {
+fn spawn_player(mut commands: Commands) {
     let player = commands
-        .spawn_bundle(
-            SpriteBundle {
-                sprite: Sprite {
-                    custom_size: Some(vec2(32.0, 64.0)),
-                    ..Default::default()
-                },
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(vec2(32.0, 64.0)),
                 ..Default::default()
             },
-        )
+            ..Default::default()
+        })
         .insert(Speed(250.0))
         .insert(PlayerCharacter)
-        .insert(Hp { current: 30, max: 30 })
-        .insert(Mp { current: 12, max: 15 })
+        .insert(Hp {
+            current: 30,
+            max: 30,
+        })
+        .insert(Mp {
+            current: 12,
+            max: 15,
+        })
         .id();
 
     let hp_bar = commands
-        .spawn_bundle((
+        .spawn((
             StatBarColor(Color::GREEN),
             StatBarEmptyColor(Color::BLACK),
-            StatBarBorder { color: Color::DARK_GRAY, thickness: 3.0 },
+            StatBarBorder {
+                color: Color::DARK_GRAY,
+                thickness: 3.0,
+            },
             StatBarValue(1.0),
-            StatBarSize { full_length: 50.0, thickness: 6.0 },
+            StatBarSize {
+                full_length: 50.0,
+                thickness: 6.0,
+            },
             StatBarSubject(player),
-            StatBarPosition(40.0 * Vec2::Y)
-        )).id();    
+            StatBarPosition(40.0 * Vec2::Y),
+        ))
+        .id();
 
     let mp_bar = commands
-        .spawn_bundle((
+        .spawn((
             StatBarColor(Color::PURPLE),
             StatBarEmptyColor(Color::BLACK),
-            StatBarBorder { color: Color::DARK_GRAY, thickness: 3.0 },
+            StatBarBorder {
+                color: Color::DARK_GRAY,
+                thickness: 3.0,
+            },
             StatBarValue(12.0 / 15.0),
-            StatBarSize { full_length: 50.0, thickness: 6.0 },
+            StatBarSize {
+                full_length: 50.0,
+                thickness: 6.0,
+            },
             StatBarSubject(player),
-            StatBarPosition(50.0 * Vec2::Y)
-        )).id(); 
+            StatBarPosition(50.0 * Vec2::Y),
+        ))
+        .id();
 
-    commands.entity(player)
-        .insert(StatBars {
-            hp: hp_bar,
-            mp: mp_bar,
-        });
+    commands.entity(player).insert(StatBars {
+        hp: hp_bar,
+        mp: mp_bar,
+    });
 }
 
 fn move_player(
     time: Res<Time>,
     keyboard: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Transform, &Speed), With<PlayerCharacter>>
+    mut query: Query<(&mut Transform, &Speed), With<PlayerCharacter>>,
 ) {
     query.for_each_mut(|(mut transform, player_speed)| {
         let mut m = Vec3::ZERO;
         if keyboard.pressed(KeyCode::A) {
             m -= Vec3::X
-        } 
+        }
         if keyboard.pressed(KeyCode::D) {
             m += Vec3::X
         }
@@ -99,15 +120,15 @@ fn update_stats(
     mut players: Query<(&mut Hp, &mut Mp), With<PlayerCharacter>>,
 ) {
     *cooldown -= time.delta_seconds();
-    if 0.0 < *cooldown { 
-        return; 
+    if 0.0 < *cooldown {
+        return;
     } else {
         *cooldown = 0.1;
     }
     players.for_each_mut(|(mut hp, mut mp)| {
         if keyboard.pressed(KeyCode::Down) {
             hp.current = (hp.current - 1).clamp(0, hp.max);
-        } 
+        }
         if keyboard.pressed(KeyCode::Up) {
             hp.current = (hp.current + 1).clamp(0, hp.max);
         }
@@ -135,10 +156,7 @@ fn update_bars(
     });
 }
 
-fn death(
-    mut commands: Commands,
-    query: Query<(Entity, &Hp)>,
-) {
+fn death(mut commands: Commands, query: Query<(Entity, &Hp)>) {
     query.for_each(|(entity, hp)| {
         if hp.current <= 0 {
             commands.entity(entity).despawn();
@@ -148,14 +166,16 @@ fn death(
 
 fn main() {
     App::new()
-    .insert_resource(ClearColor(Color::NAVY))
-    .add_plugins(DefaultPlugins)
-    .add_plugin(StatBarsPlugin)
-    .add_startup_system(|mut commands: Commands| { commands.spawn_bundle(Camera2dBundle::default()); })   
-    .add_startup_system(spawn_player)
-    .add_system(move_player)
-    .add_system(death)
-    .add_system(update_stats)
-    .add_system(update_bars)
-    .run();
+        .insert_resource(ClearColor(Color::NAVY))
+        .add_plugins(DefaultPlugins)
+        .add_plugin(StatBarsPlugin)
+        .add_startup_system(|mut commands: Commands| {
+            commands.spawn(Camera2dBundle::default());
+        })
+        .add_startup_system(spawn_player)
+        .add_system(move_player)
+        .add_system(death)
+        .add_system(update_stats)
+        .add_system(update_bars)
+        .run();
 }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,121 +1,111 @@
+use crate::prelude::*;
 use bevy::prelude::*;
+use bevy::render::texture::DEFAULT_IMAGE_HANDLE;
 use bevy::render::Extract;
 use bevy::render::RenderApp;
-use bevy::render::RenderStage;
-use bevy::render::texture::DEFAULT_IMAGE_HANDLE;
+use bevy::render::RenderSet;
 use bevy::sprite::ExtractedSprite;
 use bevy::sprite::ExtractedSprites;
 use bevy::sprite::SpriteSystem;
 use copyless::VecHelper;
-use crate::prelude::*;
 
 const DEFAULT_BAR_Z_DEPTH: f32 = 950.0;
 
 fn extract_status_bars(
     mut extracted_sprites: ResMut<ExtractedSprites>,
     subject_query: Extract<Query<&GlobalTransform>>,
-    status_bar_query: Extract<Query<(
-        &StatBarColor,
-        Option<&StatBarEmptyColor>,
-        Option<&StatBarBorder>,
-        &StatBarValue,
-        &StatBarSize,
-        &StatBarSubject,
-        Option<&StatBarPosition>,
-        Option<&StatBarZDepth>,
-        Option<&StatBarAlignment>,
-        Option<&StatBarOrientation>,
-    )>>,
+    status_bar_query: Extract<
+        Query<(
+            &StatBarColor,
+            Option<&StatBarEmptyColor>,
+            Option<&StatBarBorder>,
+            &StatBarValue,
+            &StatBarSize,
+            &StatBarSubject,
+            Option<&StatBarPosition>,
+            Option<&StatBarZDepth>,
+            Option<&StatBarAlignment>,
+            Option<&StatBarOrientation>,
+        )>,
+    >,
 ) {
     for (
-        &StatBarColor(color), 
-        empty_color_option, 
-        border_option, 
-        &StatBarValue(value), 
-        size, 
-        &StatBarSubject(subject), 
+        &StatBarColor(color),
+        empty_color_option,
+        border_option,
+        &StatBarValue(value),
+        size,
+        &StatBarSubject(subject),
         position_option,
         z_option,
         _alignment_option,
         _orientation_option,
-    ) in status_bar_query.iter() {
-        let position = position_option.map(|&StatBarPosition(p)| p).unwrap_or(Vec2::ZERO);
-        let z_depth = z_option.map(|&StatBarZDepth(z)| z).unwrap_or(DEFAULT_BAR_Z_DEPTH);
-        if let Ok(translation) =
-            subject_query
-            .get(subject)
-            .map(|subject_transform| 
-                (subject_transform.translation().truncate() + position)
-                .extend(z_depth)
-            ) {
-                
+    ) in status_bar_query.iter()
+    {
+        let position = position_option
+            .map(|&StatBarPosition(p)| p)
+            .unwrap_or(Vec2::ZERO);
+        let z_depth = z_option
+            .map(|&StatBarZDepth(z)| z)
+            .unwrap_or(DEFAULT_BAR_Z_DEPTH);
+        if let Ok(translation) = subject_query.get(subject).map(|subject_transform| {
+            (subject_transform.translation().truncate() + position).extend(z_depth)
+        }) {
             let inner_size = size.full_length * Vec2::X + size.thickness * Vec2::Y;
 
             if let Some(border) = border_option {
-                let border_size = 
-                    inner_size
-                    + border.thickness * Vec2::ONE;
+                let border_size = inner_size + border.thickness * Vec2::ONE;
 
-                extracted_sprites.sprites.alloc().init(
-                    ExtractedSprite {
-                        entity: subject,
-                        transform: GlobalTransform::from_translation(translation),
-                        color: border.color,
-                        rect: None,
-                        custom_size: Some(border_size),
-                        image_handle_id: DEFAULT_IMAGE_HANDLE.into(),
-                        flip_x: false,
-                        flip_y: false,
-                        anchor: Default::default()
-                    }
-                );
+                extracted_sprites.sprites.alloc().init(ExtractedSprite {
+                    entity: subject,
+                    transform: GlobalTransform::from_translation(translation),
+                    color: border.color,
+                    rect: None,
+                    custom_size: Some(border_size),
+                    image_handle_id: DEFAULT_IMAGE_HANDLE.into(),
+                    flip_x: false,
+                    flip_y: false,
+                    anchor: Default::default(),
+                });
             }
-            
+
             if let Some(empty_color) = empty_color_option {
-                extracted_sprites.sprites.alloc().init(
-                    ExtractedSprite {
-                        entity: subject,
-                        transform: GlobalTransform::from_translation(translation),
-                        color: empty_color.0,
-                        rect: None,
-                        custom_size: Some(inner_size),
-                        image_handle_id: DEFAULT_IMAGE_HANDLE.into(),
-                        flip_x: false,
-                        flip_y: false,
-                        anchor: Default::default()
-                    }
-                );
+                extracted_sprites.sprites.alloc().init(ExtractedSprite {
+                    entity: subject,
+                    transform: GlobalTransform::from_translation(translation),
+                    color: empty_color.0,
+                    rect: None,
+                    custom_size: Some(inner_size),
+                    image_handle_id: DEFAULT_IMAGE_HANDLE.into(),
+                    flip_x: false,
+                    flip_y: false,
+                    anchor: Default::default(),
+                });
             }
 
             if 0.0 < value {
                 let clamped_value = value.clamp(0.0, 1.0);
-                let bar_size = 
-                    clamped_value * inner_size.x * Vec2::X 
-                    + inner_size.y * Vec2::Y;
-                let bar_translation = 
-                    0.5 * size.full_length * (value - 1.0) * Vec3::X
-                    + translation;
+                let bar_size = clamped_value * inner_size.x * Vec2::X + inner_size.y * Vec2::Y;
+                let bar_translation =
+                    0.5 * size.full_length * (value - 1.0) * Vec3::X + translation;
 
-                extracted_sprites.sprites.alloc().init(
-                    ExtractedSprite {
-                        entity: subject,
-                        transform: GlobalTransform::from_translation(bar_translation),
-                        color,
-                        rect: None,
-                        custom_size: Some(bar_size),
-                        image_handle_id: DEFAULT_IMAGE_HANDLE.into(),
-                        flip_x: false,
-                        flip_y: false,
-                        anchor: Default::default()
-                    }
-                );
+                extracted_sprites.sprites.alloc().init(ExtractedSprite {
+                    entity: subject,
+                    transform: GlobalTransform::from_translation(bar_translation),
+                    color,
+                    rect: None,
+                    custom_size: Some(bar_size),
+                    image_handle_id: DEFAULT_IMAGE_HANDLE.into(),
+                    flip_x: false,
+                    flip_y: false,
+                    anchor: Default::default(),
+                });
             }
-
         }
     }
 }
 
-#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
 pub enum StatBarSystem {
     ExtractStatBars,
 }
@@ -124,14 +114,11 @@ pub struct RenderStatBarsPlugin;
 
 impl Plugin for RenderStatBarsPlugin {
     fn build(&self, app: &mut App) {
-
         if let Ok(render_app) = app.get_sub_app_mut(RenderApp) {
-            render_app
-            .add_system_to_stage(
-                RenderStage::Extract,
+            render_app.add_system(
                 extract_status_bars
-                .label(StatBarSystem::ExtractStatBars)
-                .after(SpriteSystem::ExtractSprites)
+                    .in_set(RenderSet::ExtractCommands)
+                    .after(SpriteSystem::ExtractSprites),
             );
         }
     }
